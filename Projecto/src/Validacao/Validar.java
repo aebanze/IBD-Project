be20837e5b170e9229877e5cd2bd2adc02b09a2d
Angel Banze <angeleliasanjo@gmail.com>
2021-10-31 14:26:36 -0700

    validacao

--- Projecto/src/Validacao/Validar.java
+++ Projecto/src/Validacao/Validar.java
@@ -3,4 +3,13 @@ package Validacao;
 
 public class Validar {
     
+    public boolean Usuario (String nome, String senha){
+        
+        return false;
+    }
+    
+    public boolean Vendas (String produto, int quantidade){
+        
+        return false;
+    }
 }
